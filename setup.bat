9558037967790575773e00218a1281aa2a487f13
Rajat Batra <rajatbatra@google.com>
2023-09-17 15:15:44 +0530

    Adding the setup script for windows
    Signed-off-by: Rajat Batra <rajatbatra@google.com>

---
+++ setup.bat
@@ -0,0 +1,25 @@
+
+@REM Copyright © 2023 inpyjama.com. All Rights Reserved.
+@REM Author: Rajat Batra <rajat@inpyjama.com>
+
+
+set -e
+
+echo "#1: Flushing docker cache"
+docker builder prune
+
+echo "#2: Build a linux base image"
+docker build -t c-ninja-linux_base .
+
+echo "#3: Mapping current host directory to guest's ~/Documents/ directory"
+
+docker run -h "c-ninja-linux" -t -d -P -v %cd%:/home/c-ninja/Documents/ --name c-ninja-linux c-ninja-linux_base
+
+echo "#4: Setting zsh as the default terminal on guest"
+
+docker exec -it c-ninja-linux sh -c "echo \"c\" | sudo -S usermod -s /usr/bin/zsh c-ninja"
+
+echo "#5: Populating the .bashrc file of the host with c-ninja alias"
+echo  docker exec -it c-ninja-linux /bin/zsh -c "touch ~/.zshrc; cd /home/c-ninja/Documents; zsh -i" >> c-ninja.bat
+
+echo "Setup Complete! Run the c-ninja batch file to start the container"
